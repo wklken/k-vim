1168a3c44864531e35799d25ba9cf60145952e4e
LittleKey <kk850584882@gmail.com>
2013-10-02 07:41:55 +0000

    update YCM configure file

--- others/YCM-Configure-File/_ycm_extra_conf.py
+++ others/YCM-Configure-File/_ycm_extra_conf.py
@@ -1,14 +1,14 @@
 import os
 import ycm_core
 from clang_helpers import PrepareClangFlags
- 
+
 # Set this to the absolute path to the folder (NOT the file!) containing the
 # compile_commands.json file to use that instead of 'flags'. See here for
 # more details: http://clang.llvm.org/docs/JSONCompilationDatabase.html
 # Most projects will NOT need to set this to anything; you can just change the
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
 compilation_database_folder = ''
- 
+
 # These are the compilation flags that will be used in case there's no
 # compilation database set.
 flags = [
@@ -20,19 +20,27 @@ flags = [
     '-I',
     '.',
     '-isystem',
-    '/usr/lib/c++/v1'
+    '/usr/lib/c++/v1',
+    '-lgmpxx',
+    '-lgmp',
+    '-B',
+    '/usr',
+    '-I',
+    '/usr/include',
+    '-L',
+    '/usr/lib'
 ]
- 
+
 if compilation_database_folder:
     database = ycm_core.CompilationDatabase(compilation_database_folder)
 else:
     database = None
- 
- 
+
+
 def DirectoryOfThisScript():
     return os.path.dirname(os.path.abspath(__file__))
- 
- 
+
+
 def MakeRelativePathsInFlagsAbsolute(flags, working_directory):
     if not working_directory:
         return flags
@@ -41,27 +49,27 @@ def MakeRelativePathsInFlagsAbsolute(flags, working_directory):
     path_flags = ['-isystem', '-I', '-iquote', '--sysroot=']
     for flag in flags:
         new_flag = flag
- 
+
         if make_next_absolute:
             make_next_absolute = False
             if not flag.startswith('/'):
                 new_flag = os.path.join(working_directory, flag)
- 
+
         for path_flag in path_flags:
             if flag == path_flag:
                 make_next_absolute = True
                 break
- 
+
             if flag.startswith(path_flag):
                 path = flag[len(path_flag):]
                 new_flag = path_flag + os.path.join(working_directory, path)
                 break
- 
+
         if new_flag:
             new_flags.append(new_flag)
     return new_flags
- 
- 
+
+
 def FlagsForFile(filename):
     if database:
         # Bear in mind that compilation_info.compiler_flags_ does NOT return a
@@ -75,7 +83,7 @@ def FlagsForFile(filename):
     else:
         relative_to = DirectoryOfThisScript()
         final_flags = MakeRelativePathsInFlagsAbsolute(flags, relative_to)
- 
+
     return {
         'flags': final_flags,
         'do_cache': True}
