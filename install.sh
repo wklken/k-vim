62625cff7613229634ffedcb86a1d693ebe9ad88
房文同 <fangwentong2012@gmail.com>
2014-09-05 16:08:42 +0800

    添加系统clang检测语句, fix #26

--- install.sh
+++ install.sh
@@ -43,7 +43,12 @@ echo "It will take a long time, just be patient!"
 echo "If error,you need to compile it yourself"
 echo "cd $CURRENT_DIR/bundle/YouCompleteMe/ && bash -x install.sh --clang-completer"
 cd $CURRENT_DIR/bundle/YouCompleteMe/
-bash -x install.sh --clang-completer
+if [ `whereis clang` ]   # check system clang
+then
+    bash -x install.sh --clang-completer --system-libclang   # use system clang
+else
+    bash -x install.sh --clang-completer
+fi
 
 #vim bk and undo dir
 if [ ! -d /tmp/vimbk ]
