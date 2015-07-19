6409130635848737257fdc7d5a81dc184a833bff
ox0spy <ox0spy@gmail.com>
2015-07-19 14:05:51 +0800

    use Vundle.vim: update git repository url.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 CURRENT_DIR=`pwd`
+VUNDLE_VIM_URL="https://github.com/VundleVim/Vundle.vim.git"
 
 lnif() {
     if [ -e "$1" ]; then
@@ -23,9 +24,18 @@ lnif $CURRENT_DIR/vimrc.bundles $HOME/.vimrc.bundles
 lnif "$CURRENT_DIR/" "$HOME/.vim"
 
 echo "Step3: install vundle"
-if [ ! -e $CURRENT_DIR/bundle/vundle ]; then
+# update vundle.vim git repository URL
+if [ -d $CURRENT_DIR/bundle/vundle/.git ]; then
+    mv $CURRENT_DIR/bundle/vundle $CURRENT_DIR/bundle/Vundle.vim && \
+        git remote rm origin && \
+        git remote add origin $VUNDLE_VIM_URL
+elif [ -d $CURRENT_DIR/bundle/vundle ]; then
+    rm -rf $CURRENT_DIR/bundle/vundle
+fi
+
+if [ ! -e $CURRENT_DIR/bundle/Vundle.vim ]; then
     echo "Installing Vundle"
-    git clone https://github.com/VundleVim/Vundle.vim.git $CURRENT_DIR/bundle/Vundle.vim
+    git clone $VUNDLE_VIM_URL $CURRENT_DIR/bundle/Vundle.vim
 else
     echo "Upgrade Vundle"
     cd "$HOME/.vim/bundle/Vundle.vim" && git pull origin master
