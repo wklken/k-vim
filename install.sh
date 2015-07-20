561a3d186f7fa65538caa8564de9e3a640a7d25f
ox0spy <ox0spy@gmail.com>
2015-07-20 18:54:51 +0800

    remove vundle directory, then get the Vundle.vim.

--- install.sh
+++ install.sh
@@ -23,16 +23,13 @@ lnif $CURRENT_DIR/vimrc $HOME/.vimrc
 lnif $CURRENT_DIR/vimrc.bundles $HOME/.vimrc.bundles
 lnif "$CURRENT_DIR/" "$HOME/.vim"
 
-echo "Step3: install vundle"
-# update vundle.vim git repository URL
-if [ -d $CURRENT_DIR/bundle/vundle/.git ]; then
-    mv $CURRENT_DIR/bundle/vundle $CURRENT_DIR/bundle/Vundle.vim && \
-        git remote rm origin && \
-        git remote add origin $VUNDLE_VIM_URL
-elif [ -d $CURRENT_DIR/bundle/vundle ]; then
+echo "Step3: install Vundle.vim"
+# remove vundle directory
+if [ -e $CURRENT_DIR/bundle/vundle ]; then
     rm -rf $CURRENT_DIR/bundle/vundle
 fi
 
+# get the latest Vundle.vim
 if [ ! -e $CURRENT_DIR/bundle/Vundle.vim ]; then
     echo "Installing Vundle"
     git clone $VUNDLE_VIM_URL $CURRENT_DIR/bundle/Vundle.vim
